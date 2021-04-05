5bb0233d1d5cb6833d4dbb933a13940e5ae07b41
Stacia <staciaann\.aguilar@gmail.com>
2021-04-05 15:52:00 -0700

    mad lib first draft

---
+++ Code/Stacia/lab_madlibs.py
@@ -0,0 +1,17 @@
+
+ADJ1 = input ( "adjective ")
+ADJ2 = input ( "adjective ")
+OCCUPATION= input ("job ")
+ANIMALS= input ("plural animals ")
+ANIMAL= input ("single animal ")
+NOUN1 = input ("noun")
+ADJ3 =  input ("adjective ")
+APPLIANCE = input ("appliance ")
+VERB = input ("verb ")
+NOUN2 =input ("noun ")
+YELL=  input ("exclimation ")
+NOUNS= input ("plural noun ")
+
+print (f"The {ADJ1} {ADJ2} {OCCUPATION} {ANIMALS} are 4 {ANIMALS} who are lead by a/an {ANIMAL} in the fight against {NOUN1}.
+\n They strive to protect {NOUNS} from all violence, but their most {ADJ3} enemy is the dreaded {APPLIANCE}.
+\n They also love to {VERB} {NOUN2}! {YELL}, dude!")
