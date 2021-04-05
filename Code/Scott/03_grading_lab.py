835fb52d7fcbc0359f1d3101549bd31c5f67e5b4
Scott3-0 <bremmerscottw@gmail.com>
2021-04-05 16:32:00 -0700

    Grading Lab started (WIP)

---
+++ Code/Scott/03_grading_lab.py
@@ -0,0 +1,20 @@
+gradesIn = []
+studentCount = input("How many students are you grading?: ")
+studentCount = int(studentCount)
+
+def GradeEnter():
+
+    for i in range(0, studentCount):
+        curStudent = input(f'Enter the numerical grade for student {i+1}: ')
+        gradesIn.append(curStudent)
+
+def GradeConvert():
+    for i in range(0, studentCount):
+        curStudent = gradesIn[i]
+        if():
+        elif():
+        elif():
+        
+
+
+GradeEnter()
