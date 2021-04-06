87cf3ee3e638a633d9e16c26c55754e9ff215398
Adam Meier <meieradamn@gmail.com>
2021-04-06 12:15:34 -0400

    updated lab 1 and 2, added lab 3

--- Code/Adam/Python/lab02_madlibs.py
+++ Code/Adam/Python/lab02_madlibs.py
@@ -13,6 +13,6 @@ adj3 = input('Enter another adjective: ')
 
 adj4 = input('Enter one more adjective: ')
 
-mad_lib = f'Roses are {adj1} {pl_noun} are {adj2}. I\'m really {adj3} to have {adj4} friend like you!'
+mad_lib = f'Roses are {adj1} {pl_noun} are {adj2}. I\'m really {adj3} to have a {adj4} friend like you!'
 
 print(mad_lib)
---
+++ Code/Adam/Python/lab03_grading.py
@@ -0,0 +1,29 @@
+
+
+# Part 1 - Convert a number grade to a letter grade, using if and elif 
+# statements and comparisons. First have the user enter a number representing 
+# the grade (0-100). Then convert the number grade to a letter grade.
+
+
+# num = input('Enter the number grade: ')
+# num = int(num)
+
+# if num <= 59:
+#     print('F')
+# elif num >= 60 and num <=69:
+#     print('D')
+# elif num >= 70 and num <=79:
+#     print('C')
+# elif num >= 80 and num <=89:
+#     print('B')
+# else:
+#     print('A')
+
+
+# Part 2 - Find the specific letter grade (A+, B-, etc). You can check for 
+# more specific ranges using if statements, or use modulus % to get the 
+# ones-digit to set another string to '+', '-', or ' '. Then you can 
+# concatenate that string with your grade string.
+
+num = input('Enter the number grade: ')
+num = int(num)
