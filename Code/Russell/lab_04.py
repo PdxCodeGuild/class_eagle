8467555cbafb721e7b9a94475e66784708a8971c
Russell Fraze <russell.fraze@gmail.com>
2021-04-06 14:14:37 -0700

    lab 04 added

---
+++ Code/Russell/lab_04.py
@@ -0,0 +1,18 @@
+money = float(input("How much you got?"))
+
+def convert_to_coins(a):
+    cents = a * 100
+    quarters = int(cents // 25)
+    remain = cents % 25
+    dimes = int(remain // 10)
+    remain_2 = remain % 10
+    nickles = int(remain_2 // 5)
+    remain_3 = remain_2 / 1
+    pennies = int(remain_3 / 1)
+    return quarters, dimes, nickles, pennies
+
+quarters, dimes, nickles, pennies = convert_to_coins(1.36)
+    
+print(f"Ya got {quarters} quarters, {dimes} dimes, {nickles} nickles, {pennies} pennies")
+
+  
