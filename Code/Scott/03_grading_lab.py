19398fe69dc8f6bc09a92e5074e701cea4e6c531
Scott3-0 <bremmerscottw@gmail.com>
2021-04-06 10:28:49 -0700

    Finished Grading Lab

--- Code/Scott/03_grading_lab.py
+++ Code/Scott/03_grading_lab.py
@@ -1,20 +1,46 @@
 gradesIn = []
+gradesOut = []
 studentCount = input("How many students are you grading?: ")
 studentCount = int(studentCount)
 
 def GradeEnter():
-
     for i in range(0, studentCount):
         curStudent = input(f'Enter the numerical grade for student {i+1}: ')
         gradesIn.append(curStudent)
+        gradesOut.append(curStudent)
 
-def GradeConvert():
-    for i in range(0, studentCount):
-        curStudent = gradesIn[i]
-        if():
-        elif():
-        elif():
+def GradeConvert(a):
+    curStudent = int(a)
+    gO = ''
+
+    if curStudent >= 90:
+        gO = 'A'
+    elif curStudent >= 80:
+        gO = 'B'
+    elif curStudent >= 70:
+        gO = 'C'
+    elif curStudent >= 60:
+        gO = 'D'
+    else:
+        gO = 'F'
         
+    curStudent = curStudent % 10
+    if curStudent >= 8:
+        gO = f'{gO}+'
+    elif curStudent < 5:
+        gO = f'{gO}-'
+
+    return gO
+
 
 
 GradeEnter()
+classAvg = 0
+line = '---------------------------\n'
+print(line)
+for i in range(0, studentCount):
+    gradesOut[i] = GradeConvert(gradesIn[i])
+    print(f'Student {i+1}: {gradesIn[i]} | {gradesOut[i]}')
+    classAvg += int(gradesIn[i])
+classAvg /= studentCount
+print(line + f'Class Avg: {classAvg} | {GradeConvert(classAvg)}')
