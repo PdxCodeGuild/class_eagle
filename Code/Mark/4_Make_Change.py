b737f87074944bccde217e1954d76be38e209a9f
Mark <meherin.mark@gmail.com>
2021-04-06 13:56:03 -0700

    make change update

--- Code/Mark/4_Make_Change.py
+++ Code/Mark/4_Make_Change.py
@@ -5,13 +5,14 @@ import math
 def make_change(coin_amount):
     
     coin_amount = math.ceil(user_m/.01)
-    quart = coin_amount//25
+
+    quart = coin_amount//25 # The amount of quarters
     coin_amount = coin_amount - (quart*25)
-    dime = coin_amount//10
+    dime = coin_amount//10 # The amount of dimes
     coin_amount = coin_amount - (dime*10)
-    nick = coin_amount//5
+    nick = coin_amount//5 # The amount of nickels
     coin_amount = coin_amount - (nick*5)
-    pen = coin_amount//1
+    pen = coin_amount//1 # The amount of pennies
 
     return print(f'''
     Your change is:
@@ -29,4 +30,4 @@ while True:
     else:
         break
 
-print('Thank you')
+print('Thank you')      
