730fd9b15d66143095ff1bc3e4825b9649d6be92
Mark <meherin.mark@gmail.com>
2021-04-09 12:04:05 -0700

    peaks and values

--- Code/Mark/7_PeaksnValleys.py
+++ Code/Mark/7_PeaksnValleys.py
@@ -1,7 +1,6 @@
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 
-
 # def peak(data):
 #     output = []
 #     for i in range(len(data)-1):
@@ -9,8 +8,7 @@ data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 #           output.append(i)
 #     return output
 
-            
-        
+
 # def valley(data):
 #     output = []
 #     for i in range(1,len(data)):
@@ -22,14 +20,12 @@ data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def peaks_and_valleys(data):
     output = []
-    for i in range(1,len(data)-1):
+    for i in range(1, len(data)-1):
         if data[(i-1)] > data[i] < data[(i+1)] or data[(i-1)] < data[i] > data[(i+1)]:
-          output.append(i)
+            output.append(i)
     for i in data:
-        print('X'*i)
+        print('x '*i)
     return output
-
-   
 
 
 # print(peak(data))
