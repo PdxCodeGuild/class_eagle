52dcdcf35f5c23e4d27ee5689f5b4f3f7ca83e33
Mark <meherin.mark@gmail.com>
2021-04-09 16:09:57 -0700

    pick6

---
+++ Code/Mark/8_pick6.py
@@ -0,0 +1,67 @@
+from random import *
+
+# returns a list of 6 random numbers between 1 and 99
+def pick6(num_list):
+    num_list = []
+    for i in range(6):
+        num_list.append(randint(1,99))
+    return num_list
+
+
+# determines the amount of matches
+def num_matches(win_nums,ticket):
+    matches = 0
+    for i in range(0,len(win_nums)):
+        if win_nums[i] == ticket[i]:
+            matches += 1
+    return matches
+
+
+# creating the blank variables and lists
+user_balance = 0
+total_earnings = 0
+expense = 0
+ticket = []
+win_nums = []
+
+# Loop to determine to amount won
+for i in range(100000):
+
+    user_balance -=2
+    expense += 2
+    ticket = pick6(ticket)
+    win_nums = pick6(win_nums)
+    matches = num_matches(win_nums,ticket)
+    if matches == 0:
+        continue
+    elif matches == 1:
+        user_balance += 4
+        total_earnings += 4
+    elif matches == 2:
+        user_balance += 7
+        total_earnings += 7
+    elif matches == 3:
+        user_balance += 100
+        total_earnings += 100
+    elif matches == 4:
+        user_balance += 50000
+        total_earnings += 50000
+    elif matches == 5:
+        user_balance += 1000000
+        total_earnings += 1000000
+    elif matches == 6:
+        user_balance += 25000000
+        total_earnings += 25000000
+
+# determining the return on investment
+roi = (total_earnings-expense)/expense
+
+# displaying the results to the user
+print(f'Total expense: $ {expense}')
+print(f'Total earnings: $ {total_earnings}')
+print(f'Current balance: $ {user_balance}')
+print(f'Your return on investment is {roi}')
+
+
+
+
