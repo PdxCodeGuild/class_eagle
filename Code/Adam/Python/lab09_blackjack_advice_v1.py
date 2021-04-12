bcecc2f567ed9a4ea9e94fd5a84ca171a4569c8e
Adam Meier <meieradamn@gmail.com>
2021-04-12 16:00:34 -0400

    lab09 updated

--- Code/Adam/Python/lab09_blackjack_advice_v1.py
+++ Code/Adam/Python/lab09_blackjack_advice_v1.py
@@ -18,20 +18,15 @@ Print out the current total point value and the advice.
 '''
 
 
+# 2) figure out the point value of each card individually. Number cards are 
+# worth their number, all face cards are worth 10. At this point, assume aces 
+# are worth 1.
 def card_value(card):
     face_cards = {'A':1, '1':1, '2':2, '3':3, '4':4, '5': 5, '6':6, '7':7, '8':8, '9':9, 'J':10, 'Q':10, 'K':10}
     return face_cards[card]
 
-# face_cards = {'A': 1, 'J': 10, 'Q': 10, 'K': 10}
-
-face_cards = {'A': 1, 'J': 10, 'Q': 10, 'K': 10}
-hand_total = 0
 
 # 1) Ask the user for three playing cards (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K).
-# 2) figure out the point value of each card individually. Number cards are 
-# worth their number, all face cards are worth 10. At this point, assume aces 
-# are worth 1.
-
 card_one = card_value(input("\nWhat's your first card? ").upper())
 card_two = card_value(input("\nWhat's your second card? ").upper())
 card_three = card_value(input("\nWhat's your third card? ").upper())
