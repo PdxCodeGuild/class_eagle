bae7215af1397a759f12a49709fe4e16e09d3b3a
Mark <meherin.mark@gmail.com>
2021-04-13 11:51:21 -0700

    dad jokes

--- Code/Mark/10_DadJoke.py
+++ Code/Mark/10_DadJoke.py
@@ -2,23 +2,35 @@ from os import * # imported for the "cls"
 import requests 
 import time
 
+while True:
+    url = 'https://icanhazdadjoke.com/search?term='
 
+    term = input('Keyword to search for: ')
 
+    # importing the dad joke from the internet
+    response = requests.get(url+term, headers={'Accept': 'application/json'})
 
-# importing the dad joke from the internet
-response = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
+    # assigning it to the dict 'dad_joke'
+    dad_joke = response.json()
 
-# assigning it to the dict 'dad_joke'
-dad_joke = response.json()
+    # clearing the screen to make it look better
+    system('cls')
 
-# clearing the screen to make it look better
-system('cls')
+    # using a timer to make it seem like its loading for suspense
+    print('Loading dadjokes.exe...')
+    time.sleep(6)
 
-# using a timer to make it seem like its loading for suspense
-print('Loading dadjoke.exe...')
-time.sleep(6)
+    # clearing the screen of the loading text and presenting the dad joke
+    system('cls')
 
-# clearing the screen of the loading text and presenting the dad joke
-system('cls')
-print(dad_joke['joke'])
-time.sleep(6)
+    # setting results as a dictionary for the results from the search
+    results = dad_joke['results']
+    
+    # iterating through the list of dictionaries to print the results of the search
+    for i in results:
+        print(i['joke'])
+
+    answer = input('Would you like to search another term? ')
+
+    if answer != 'yes':
+        break
