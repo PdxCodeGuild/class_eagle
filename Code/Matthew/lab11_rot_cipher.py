c0b1022ff1c26afdd38dd7d3264bb994720c62dd
flux2341 <flux2341@gmail.com>
2021-04-14 11:35:47 -0700

    added unit converter

--- Code/Matthew/lab11_rot_cipher.py
+++ Code/Matthew/lab11_rot_cipher.py
@@ -46,11 +46,32 @@ print(rot13('hello')) # uryyb
 
 
 
+import string
+
+def rotn(text, rot_amount):
+    alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation + string.whitespace
+    output = ''
+    for char in text:
+        index = alphabet.find(char)
+        if index == -1:
+            output += char
+        else:
+            index += rot_amount
+            index %= len(alphabet)
+            output += alphabet[index]
+    return output
+        
+print(rotn('Hello World!', 13))
+print(rotn(rotn('Hello World!', 13), -13))
 
 
 
 
-
-
+encrypted_text = 'UryyBh9BEyq.'
+for i in range(26):
+    decrypted_text = rotn(encrypted_text, -i)
+    if 'Hello' in decrypted_text:
+        print('The decrypted text is: ' + decrypted_text)
+        print('The rotation amount was ' + str(i))
 
 
