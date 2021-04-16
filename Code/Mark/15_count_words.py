504f421dd434bd54b529d5eb4747fa491046c8cd
Mark <meherin.mark@gmail.com>
2021-04-16 10:03:51 -0700

    count words v3

--- Code/Mark/15_count_words.py
+++ Code/Mark/15_count_words.py
@@ -6,17 +6,24 @@ response = requests.get('http://www.gutenberg.org/files/65075/65075-0.txt')
 response.encoding = 'utf-8' # set encoding to utf-8
 book = response.text
 
-book = re.findall(r'([A-Za-z]+ [A-Za-z]+)',book)
+
+# asks the user for the keyword to search by
+user_word = input('Select a keyword to search for: ')
 
 
+# raw string to find all uses of the user word and the word that follows it
+book = re.findall(fr'({user_word} [A-Za-z]+)',book, re.IGNORECASE)
+
+
+# a for loop to iterate through each string and take count of each time it appears
 count = 0
 word_count = {}
 for words_2 in book:
     words_2 = words_2.lower()
     if words_2 in word_count:
         continue
-    count_word = book.count(words_2)
-    word_count[words_2] = count_word
+    count_word = book.count(words_2) 
+    word_count[words_2] = count_word # adds the selected string and its count as key:values pairs in a dictionary
 
 
 
