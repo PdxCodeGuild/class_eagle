4801c418b2db51126fa30552055dfc83d40042bc
flux2341 <flux2341@gmail.com>
2021-04-16 11:18:14 -0700

    added count words solution

---
+++ Code/Matthew/practice_whatever.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+# Fibonacci
+# Write a function that takes `n` as a parameter, and returns a list containing the first `n` [Fibonacci Numbers](https://en.wikipedia.org/wiki/Fibonacci_number).
+
+def fibonacci(n):
+    ...
+# print(fibonacci(8)) # [1, 1, 2, 3, 5, 8, 13, 21]
+
+
+
+# Find Pair
+# Given a list of numbers, and a target number, find a pair of numbers from the list that sum to a target number. Optional: return a list of all pairs of numbers that sum to a target value.
+
+
+def find_pair(nums, target):
+  ...
+# print(find_pair([5, 6, 2, 3], 7)) # [5, 2]
+
+
+
+# Scramble Letters
+# Write a function `scramble_letters` to scramble the internals letters of words, but keep first and last letter the same.
+
+def scramble_letters(text):
+    ...
+# print(scramble_letters('hello world')) # hlelo wlrod
+# print(scramble_letters('this is hard to read')) # tihs is hrad to raed
+
