3b623ac371e4e7d5a0eb88bed3efa952a67017bc
Adam Meier <meieradamn@gmail.com>
2021-04-16 12:53:53 -0400

    added frankenstein.txt, added lab13, updated lab12

--- Code/Adam/Python/lab12_unit_converter_v4.py
+++ Code/Adam/Python/lab12_unit_converter_v4.py
@@ -13,22 +13,15 @@ Ask the user for the starting unit, the distance, and the unit to convert to.
 '''
 
 # write a function that takes a given unit and distance and converts it to meters
-
-
-def convert_to_meters(unit, distance):
+def convert_to_meters(stat_unit, end_unit, distance):
     #   dictionary of units and their conversion factor for meters
     units = {'inches': .0254, 'feet': .3048, 'yards': .9144,
              'meters': 1, 'miles': 1609.34, 'kilometers': 1000}
-    # convert distance to metes and assign to output
-    output = distance*units[unit]
+    unit_to_meters = distance*units[start_unit]
+    meters_to_unit = unit_to_meters/units[end_unit]
+    output = meters_to_unit
     return round(output, 4)
 
-# write a function that converts a distance in meters to other units of distance
-def convert_meters(unit, distance):
-    units = {'inches': .0254, 'feet': .3048, 'yards': .9144,
-             'meters': 1, 'miles': 1609.34, 'kilometers': 1000}
-    output = distance/units[unit]
-    return round(output, 4)
 
 # write a funtion to check if input is valid
 def interpret_input(unit):
@@ -40,25 +33,29 @@ def interpret_input(unit):
         'meters': ['m', 'meter', 'meters'], 
         'miles': ['mi', 'mile', 'miles'], 
         'kilometers': ['km', 'kilometer', 'kilometers']}
-    for key in valid_units:
+    for key in valid_units:     # problem
         for value in valid_units[key]:
             if unit == value:
                 output = key[0]
             else:
-                output = 'invalid unit'
+                output = 'invalid'
     return output
 
+while True:
+    output = 0
+    start_unit = input('\nWhat is the starting unit of distance? ').lower()
+    distance = float(input('\nWhat is the distance? '))
+    end_unit = input('\nWhat unit of distance would you like to convert to? ').lower()
+    # if interpret_input(start_unit) == 'invalid':
+    #     print(f'\n{start_unit} is not a valid unit')
+    #     continue
+    # elif interpret_input(end_unit) == 'invalid':
+    #     print(f'\n{end_unit} is not a vailid unit')
+    #     continue
 
-# print(convert_meters('feet', 1)) # 1
-output = 0
-start_unit = input('\nWhat is the starting unit of distance? ').lower()
-distance = float(input('\nWhat is the distance? '))
-end_unit = input(
-    '\nWhat unit of distance would you like to convert to? ').lower()
+    # convert the starting unit and distance into meters, then the distance in meters to the end unit
+    # converted_distance = convert_meters(end_unit, (convert_to_meters(start_unit, distance)))
+    converted_distance = convert_to_meters(start_unit, end_unit, distance)
+    output = f'\n{distance} {start_unit} is {converted_distance} {end_unit}'
 
-# convert the starting unit and distance into meters, then the distance in meters to the end unit
-converted_distance = convert_meters(
-    end_unit, (convert_to_meters(start_unit, distance)))
-output = f'\n{distance} {start_unit} is {converted_distance} {end_unit}'
-
-print(output)
+    print(output)
