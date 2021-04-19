b1b3b55c9208d1c6b92487168ab6414c0f27cec7
Adam Meier <meieradamn@gmail.com>
2021-04-19 17:39:43 -0400

    lab14 updated

--- Code/Adam/Python/lab14_ari.py
+++ Code/Adam/Python/lab14_ari.py
@@ -17,11 +17,12 @@ import math
 response = requests.get('http://www.gutenberg.org/files/84/84-0.txt')
 response.encoding = 'utf-8' # set encoding to utf-8
 text = response.text    # assign response to text
+# print(text)
 
 
 def clean_ebook(text):
     # https://regex101.com/r/mOQE0o/1
-    regex_ebook_start = r'(\*{3})\s(\w{5})\s(\w+)\s(\w+)\s(\w+)\s(\w+)\s(\w+)\s(...+)(\*{3})'
+    regex_ebook_start = r'(\*{3}\s\w{5}\s\w{2}\s\w{3}\s\w{7}\s\w{9}\s\w{5}\s)(...+)(\*{3})'
     # https://regex101.com/r/szSOo2/1
     regex_ebook_end = r'(\*{3})\s(\w{3})\s(\w+)\s(\w+)\s(\w+)\s(\w+)\s(\w+)\s(...+)(\*{3})'
     start_of_ebook = re.search(regex_ebook_start, text).end()
@@ -30,19 +31,14 @@ def clean_ebook(text):
     return text
 
 
-ebook = clean_ebook(text)
+# write a function that finds the ebook title.
+def get_ebook_title(text):
+    title = ''
+    regex_ebook_start = r'(\*{3}\s\w{5}\s\w{2}\s\w{3}\s\w{7}\s\w{9}\s\w{5}\s)(...+)(\*{3})'
+    result = re.search(regex_ebook_start, text)
+    title = result.group(2)
+    return title
 
-
-# # write a function that finds the ebook title.
-# def get_ebook_title(text):
-#     title = ''
-#     # https://regex101.com/r/ukBjvJ/1
-#     result = re.match(r'(\*{3}\s\w{5}\s\w{2}\s\w{3}\s\w{7}\s\w{9}\s\w{5}\s)(...+)(\*{3})', text)
-#     title = result.group(2)
-#     print(title)
-#     return title
-
-# print(get_ebook_title(text))
 
 # write a function that returns num_characters. Total letters and numbers.
 def count_characters(text):
@@ -51,8 +47,6 @@ def count_characters(text):
     count = len(char_list)
     return count
 
-display_char_count = "{:,}".format(count_characters(ebook))
-print(f'Total characters: {display_char_count}')
 
 # write a function that returns num_of_words. Number of spaces.
 def count_words(text):
@@ -61,8 +55,6 @@ def count_words(text):
     count = len(word_list)
     return count
 
-display_word_count = "{:,}".format(count_words(ebook))
-print(f'Total words: {display_word_count}')
 
 # write a function that returns num_senctences. Number of '.', '!', '?'.
 def count_sentences(text):
@@ -71,8 +63,14 @@ def count_sentences(text):
     count = len(sentences_list)
     return count
 
-display_sent_count = "{:,}".format(count_sentences(ebook))
-print(f'Total sentences: {display_sent_count}')
+
+# this function takes the number of characters, words, sentences and returns the ari
+def calculate_ari(characters, words, sentences):
+    ari = 0
+    ari = math.ceil(4.71*(characters/words) + .5*(words/sentences)-21.43)
+    if ari > 14: 
+        ari = 14
+    return ari
 
 
 # Once you’ve computed the ARI score, you can use the following dictionary to 
@@ -94,16 +92,16 @@ ari_scale = {
     14: {'ages': '18-22', 'grade_level':      'College'}
 }
 
-# store the number of characters, words, and sentences
+
+ebook = clean_ebook(text)
+ebook_title = get_ebook_title(text)
 num_of_characters = count_characters(ebook)   # number of letters and numbers
 num_of_words = count_words(ebook)   # number of spaces
 num_of_sentences = count_sentences(ebook)    # number of '.', '!', and '?'
+ari = calculate_ari(num_of_characters, num_of_words, num_of_sentences)
 
-ari = math.ceil(4.71*(num_of_characters/num_of_words) + .5*(num_of_words/num_of_sentences)-21.43)
 
-if ari > 14: 
-    ari = 14
-
-print(ari_scale[ari])
-
+print(f'\nThe ARI for {ebook_title} is {ari}')
+print(f'This corresponds to a {ari_scale[ari]} level of difficulty')
+print(f'that is suitable for an average person {ari_scale[ari]} year old.\n')
 
