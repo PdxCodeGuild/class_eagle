3789c241ff9629f19bdb816edacf32cd913e133c
Russell Fraze <russell.fraze@gmail.com>
2021-04-18 22:01:41 -0700

    rot 13 added

---
+++ Code/Russell/count_words.py
@@ -0,0 +1,37 @@
+import re
+import string
+
+
+with open('Arabian_Nights.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+
+text = text.lower() 
+punc = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~""" # remember Matthews tip here 
+for char in text:
+    if char in punc:
+        text = text.replace(char, '')
+
+regex =  r'\w+'
+
+result = re.findall(regex, text)
+
+
+arabian_words = {}
+for i in range(len(result)):
+    if result[i] not in arabian_words:
+        arabian_words[result[i]]=1
+    elif result[i] in arabian_words:
+        arabian_words[result[i]]+=1
+
+
+# print(arabian_words)
+
+words = list(arabian_words.items()) # .items() returns a list of tuples
+words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+    print(words[i])
+
+
+
+
