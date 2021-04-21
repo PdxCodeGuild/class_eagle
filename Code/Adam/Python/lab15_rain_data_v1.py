138b083fd60364e7c17f10150d5b966aa5b745a3
Adam Meier <meieradamn@gmail.com>
2021-04-20 21:14:08 -0400

    updated lab15, added hayden_island_rain.txt

---
+++ Code/Adam/Python/lab15_rain_data_v1.py
@@ -0,0 +1,78 @@
+'''
+Lab 15: Rain Data
+
+The 'City of Portland Bureau of Environmental Services' operates and maintains 
+a network of rain gauges around Portland, and publishes their data publicly at 
+http://or.water.usgs.gov/non-usgs/bes/. 
+
+Version 1 - Download or use requests to get one of these files. The two columns 
+that are most important are the date and the daily total. The simplest 
+representation of this data is a list of tuples, but you may also use a list of 
+dictionaries, or a list of instances of a custom class.
+'''
+
+import requests
+from datetime import datetime
+import re
+
+
+response = requests.get('https://or.water.usgs.gov/non-usgs/bes/hayden_island.rain')
+text = response.text
+
+# write a function that returns a list of dictionaries
+def get_dates_and_totals(text):
+    dates_totals_dic = []
+    # https://regex101.com/r/ELDlmL/1
+    pertinent_data = r'(\d{2}[-]\w{3}[-]\d{4})\s+(\d+)'
+    # capture group 1: date, capture group 2: rain_total
+    result = re.findall(pertinent_data, text) # returns a list of dates and totals
+    for i in range(len(result)):
+        date = str(datetime.strptime(result[i][0], '%d-%b-%Y').date())
+        if result[i][1] != '-':
+            rain_total = int(result[i][1])
+            dates_totals_dic.append({"date": date, "total": rain_total}) 
+            # dates_totals_dic.append({date: rain_total}) # the date is the key and the rain total is its value
+        else:
+            continue
+    return dates_totals_dic
+
+
+'''
+Version 2 - Calculate the mean, the variance, and find the day which had the most rain.
+'''
+
+# write a function that calculates and returns the mean
+def calculate_mean(data):
+    total = 0
+    mean = 0
+    for i in range(len(data)):
+        total += data[i]['total']
+    mean = total/len(data)  # The mean is the sum of all the daily totals divided by the number of daily totals.
+    return mean
+
+
+# write a function that calculates and returns the variance. Find the squared difference from the mean for each data value.
+def calculate_variance(mean, data):
+    variance = 0
+    # Subtract the mean from each data value and square the result.
+    # Find the sum of all the squared differences. ..
+    return variance
+
+
+# write a function that returns the date with the most total rain
+def most_daily_rain(data):
+    date = ''
+    for i in range(len(data)):
+        # if total is bigger than all the totals
+        # return the date for that total
+        # figure out how to sort a list of dictionaries
+        ...
+    return date
+
+
+rain_data = get_dates_and_totals(text)
+mean = calculate_mean(rain_data)
+rainiest_day = most_daily_rain(rain_data)
+variance = calculate_variance(rain_data)
+
+
