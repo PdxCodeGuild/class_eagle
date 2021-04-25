c832a0195022ea9ccec1af693853699b4c3ed36a
Andy Tran <tranandy919@gmail.com>
2021-04-24 19:26:03 -0700

    lab 6 update

--- Code/Andy/lab6_password.py
+++ Code/Andy/lab6_password.py
@@ -3,21 +3,22 @@ import string
 
 
 
-print("***** Password Generator *****")
+print("\n***** Password Generator *****\n")
 
 run_again = 'y'
 while run_again == 'y':
     chars = string.ascii_letters + string.digits + string.punctuation
-
     a = int(input("How many characters do you want for your password? "))
 
-    i = 1
-
-    while i <= a:
-        password = random.choice(chars)
-        print(password)
+    i = 0
+    password = []
+    while i < a:
+        password.append(random.choice(chars))
         i += 1
+        while i == a:
+            print(''.join(password))
+            break
+
     run_again = input('Run again? y/n ')
 else:
     print("Goodbye!")
-
