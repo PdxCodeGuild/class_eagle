8cf3afed8d39851ec80c08b3bef25c19d4b1eb3f
Andy Tran <tranandy919@gmail.com>
2021-05-05 13:38:13 -0700

    updates

--- Code/Andy/lab10_DadJokes.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'https://icanhazdadjoke.com/'
-
-response = requests.get(url, headers={'Accept' :'applications/json'})
-data = response.json()
-joke = data['joke']
-print(joke)
-
