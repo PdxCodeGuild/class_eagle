a1bec57eb196815e15ca8b7131a10c3b174865ab
Russell Fraze <russell.fraze@gmail.com>
2021-05-16 19:54:52 -0700

    flask lab updated

--- Code/Russell/HTML + CSS + Flask/flask_lab/app.py
+++ Code/Russell/HTML + CSS + Flask/flask_lab/app.py
@@ -1,10 +1,34 @@
 from flask import Flask,render_template,request
+import string
+
+alph_1 = string.ascii_lowercase
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    print(request.form['uncoded_text'])
-    return render_template('index.html')
+    if request.method == 'POST':
+        word = request.form['uncoded_text']
+        enc_word = []
+        offset = 3
+        for char in word:
+            enc_word.append(alph_1.find(char) + offset)
+
+        for i in range(len(enc_word)):
+            if enc_word[i] >= len(alph_1):
+                enc_word[i] = enc_word[i] - len(alph_1)
+
+        code_word = ''
+        for i in enc_word:
+            code_word += alph_1[i]
+
+        print(code_word)
+
+        return render_template('index.html', code_word = code_word)
+    else:
+        return render_template('index.html')
+
+
 
 app.run(debug=True)
 
