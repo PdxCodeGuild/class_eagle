059e686c203f1c9bd9d800593d564f72fc8eafa3
Russell Fraze <russell.fraze@gmail.com>
2021-05-20 13:45:30 -0700

    complete

--- Code/Russell/3 Django/lab1_django_redo/lab1_app/views.py
+++ Code/Russell/3 Django/lab1_django_redo/lab1_app/views.py
@@ -11,11 +11,17 @@ def index(request):
         password = ''
         for i in range(length):
             password += random.choice(string.ascii_letters)
-    print(password)
+        context={
+            'password': password         
+            }
+    # print(password)
+    # print(context)
+        return render(request,'lab1_app/index.html',context)
 
-
-
-        
-    return render(request,'lab1_app/index.html')
+    else:
+        context={
+            'password': ''         
+            }
+        return render(request,'lab1_app/index.html',context)
 
 
