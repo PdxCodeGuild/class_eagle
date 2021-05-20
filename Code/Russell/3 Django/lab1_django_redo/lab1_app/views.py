9ba2e99ad01d0331fb70764f9f1a1cabb4cb6a15
Russell Fraze <russell.fraze@gmail.com>
2021-05-20 11:41:32 -0700

    create password added

--- Code/Russell/3 Django/lab1_django_redo/lab1_app/views.py
+++ Code/Russell/3 Django/lab1_django_redo/lab1_app/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import string 
+import random
 
 def index(request):
     print(request.POST)
+    if request.method == "POST":
+        length = request.POST['length']
+        length = int(length)
+        password = ''
+        for i in range(length):
+            password += random.choice(string.ascii_letters)
+    print(password)
+
+
+
+        
     return render(request,'lab1_app/index.html')
 
 
