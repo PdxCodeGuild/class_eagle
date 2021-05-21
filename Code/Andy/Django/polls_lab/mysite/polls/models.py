ffbf3a88e519ab8f9bc79255fd0cde9ec1343749
Andy Tran <tranandy919@gmail.com>
2021-05-21 16:14:05 -0700

    May 21 updates

--- Code/Andy/Django/polls_lab/mysite/polls/models.py
+++ Code/Andy/Django/polls_lab/mysite/polls/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
     def __str__(self):
         return self.question_text
@@ -14,4 +19,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return self.question.question_text + ' ' + self.choice_text
