f19db3d94ab9cc8e76296674a3cdd8a4da90277e
Adam Meier <meieradamn@gmail.com>
2021-05-25 13:46:05 -0400

    todo update

--- Code/Adam/Django/todoproj/todoapp/views.py
+++ Code/Adam/Django/todoproj/todoapp/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Priority, TodoItem
 from datetime import datetime
 
 
+
 def index(request):
     todo_items = TodoItem.objects.all()
+    priorities = Priority.objects.all()
     context = {
         'title': 'Todo List',
         'todo_items': todo_items,
+        'priorities': priorities,
     }
     return render(request, 'todoapp/index.html', context)
 
 
-def save_todo_item():
-    ...
+def create(request):
+    print(request.POST)
+    new_task = request.POST['new_task']
+    priority = request.POST['priority']
+    todo_item = TodoItem(new_task=new_task, priority=priority)
+    todo_item.save()
+    return HttpResponseRedirect(reverse('todoapp:index'))
