f7e1215610979fcab9e7cd18558f3166fe8d249d
Adam Meier <meieradamn@gmail.com>
2021-05-25 16:34:51 -0400

    todo update

--- Code/Adam/Django/todoproj/todoapp/models.py
+++ Code/Adam/Django/todoproj/todoapp/models.py
@@ -12,9 +12,9 @@ class Priority(models.Model):
 
 class TodoItem(models.Model):
     text = models.CharField(max_length=200)
-    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
-    created_date = models.DateTimeField()
-    # completed_date = models.DateTimeField()
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE, related_name='todo_items')
+    created_date = models.DateTimeField(auto_now_add=True)
+    completed_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.text
