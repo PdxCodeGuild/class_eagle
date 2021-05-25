5e38a3fa8a2a5a46d0bba25eb869aa761b310e15
Andy Tran <tranandy919@gmail.com>
2021-05-25 13:32:36 -0700

    May 25th

--- Code/Andy/Django/lab_02ToDo/ToDo/models.py
+++ Code/Andy/Django/lab_02ToDo/ToDo/models.py
@@ -1,14 +1,18 @@
+from typing import Text
 from django.db import models
 from django.utils import timezone
  
+
+class Priority(models.Model):
+    pri = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.pri
+
+
 class TodoItem(models.Model):
     task = models.TextField(max_length=100)
+    priority = models.ForeignKey(Priority,null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.task
- 
-class Priority(models.Model):
-    priority = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.priority
