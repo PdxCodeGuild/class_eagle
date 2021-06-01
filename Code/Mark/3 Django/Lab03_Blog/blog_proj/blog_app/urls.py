6cfa40ff9b486e94beac3a301fd3fe89b12354c3
Mark <meherin.mark@gmail.com>
2021-06-01 09:32:59 -0700

    Blog App

--- Code/Mark/3 Django/Lab03_Blog/blog_proj/blog_app/urls.py
+++ Code/Mark/3 Django/Lab03_Blog/blog_proj/blog_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout, name='logout'),
+    path('create/', views.create, name='create'),
 ]
--- Code/Mark/3 Django/Lab03_Blog/blog_proj/blog_app/views.py
+++ Code/Mark/3 Django/Lab03_Blog/blog_proj/blog_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+import django.contrib.auth
 from django.contrib.auth.decorators import login_required
 
 def index(request):
@@ -21,14 +21,24 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        user = django.contrib.auth.authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            django.contrib.auth.login(request,user)
             return HttpResponseRedirect(reverse('blog_app:index'))
+        else:
+            return render(request, 'blog_app/login.html', {'error': 'Incorrect username or password'})
     else:
         return render(request, 'blog_app/login.html')
 
 
 @login_required
 def profile(request):
+    return render(request, 'blog_app/profile.html')
+
+def logout(request):
+    django.contrib.auth.logout(request)
+    return HttpResponseRedirect(reverse('blog_app:login'))
+
+def create(request):
+    print(request.POST)
     return render(request, 'blog_app/profile.html')
