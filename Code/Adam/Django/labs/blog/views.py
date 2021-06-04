6ff6cf5f7885652e0e36ec1cf267765f0a6e451e
Adam Meier <meieradamn@gmail.com>
2021-06-03 21:22:13 -0400

    03 blog updated templates

--- Code/Adam/Django/labs/blog/views.py
+++ Code/Adam/Django/labs/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Blogpost
+
 
 def index(request):
-    return render(request, 'blog/index.html')
-
-
-
+    all_posts = Blogpost.objects.all()
+    context = {
+        'all_posts': all_posts,
+    }
+    print(all_posts)
+    return render(request, 'blog/index.html', context)
--- Code/Adam/Django/labs/users/urls.py
+++ Code/Adam/Django/labs/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
