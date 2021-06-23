d64d6cbf458671cb82d81ef5e932f12036a9d999
Mark <meherin.mark@gmail.com>
2021-06-22 17:05:04 -0700

    Lab 05 PokeDex

---
+++ Code/Mark/4 JavaScript/Lab05_Poke_API/pokeproj/pokeapp/management/commands/load_pokemon.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from pokeapp.models import Pokemon
+import json
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        Pokemon.objects.all().delete()
+        with open('pokeapp/management/commands/pokemon.json') as file:
+            text = file.read()
+        data = json.loads(text)
+        pokemon_data = data['pokemon']
+        for pokemon in pokemon_data:
+            name = pokemon['name']
+            number = pokemon['number']
+            height = pokemon['height']
+            weight = pokemon['weight']
+            image_front = pokemon['image_front']
+            image_back = pokemon['image_back']
+            type = pokemon['types']
+            pokemon = Pokemon(name=name,number=number,height=height,weight=weight,image_front=image_front,image_back=image_back,type=type)
+            pokemon.save()
