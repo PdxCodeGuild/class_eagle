da3f29e1d7c2d7b5cdab0c04f6f09f4624c0e380
Matthew Cooper <flux2341@gmail.com>
2021-06-23 14:30:47 -0700

    added create and delete to contacts project

--- 4 JavaScript/solutions/contactspaproj/contacts/urls.py
+++ 4 JavaScript/solutions/contactspaproj/contacts/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'contacts'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contacts/', views.contacts, name='contacts')
+    path('contacts/', views.contacts, name='contacts'),
+    path('create/', views.create, name='create'),
+    path('delete/', views.delete, name='delete'),
 ]
 
