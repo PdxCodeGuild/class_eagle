22757743fb1b4b1788f0e2036007283d5d2131cf
Stacia <staciaAnn.aguilar@gmail.com>
2021-07-30 16:42:23 -0700

    comit

--- Code/Stacia/java/05 Pokelab/pokeLab/pokeappi/models.py
+++ Code/Stacia/java/05 Pokelab/pokeLab/pokeappi/models.py
@@ -12,7 +12,7 @@ class Pokemon(models.Model):
     image_front= models.CharField(max_length=5000, default='')
     image_back= models.CharField(max_length=5000, default='')
     # types= models.CharField(max_length=5000, default='')
-    
+     
     def __str__(self):
         return self.Pokemon
 
