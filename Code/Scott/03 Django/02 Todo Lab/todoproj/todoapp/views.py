79df7d8ba4444d4e2b6edcb560b01e6ce1b26add
Scott3-0 <bremmerscottw@gmail.com>
2021-08-02 12:23:19 -0700

    Django Todo App completed

--- Code/Scott/03 Django/02 Todo Lab/todoproj/todoapp/views.py
+++ Code/Scott/03 Django/02 Todo Lab/todoproj/todoapp/views.py
@@ -3,11 +3,23 @@ from .models import Priority, TodoItem
 
 # Create your views here.
 def index(request):
-    todo_list = TodoItem.objects().order_buy('-completed_date')
+    todo_list = TodoItem.objects.order_by('-completed_date')
     context = {'todo_list': todo_list}
     return render(request, 'todoapp/index.html', context)
 
 def submit(request):
     todo_item = TodoItem()
+    priority = Priority()
+    
     todo_item.text = request.POST['textInput']
+
+    priority.name = request.POST['priorityInput']
+    todo_item.priority = priority
+
+    priority.save()
+    todo_item.save()
+    
+    print(request.POST)
+    context = {'todo_item': todo_item}
+    return render(request, 'todoapp/submit.html', context) 
 #TODO: fix this view
