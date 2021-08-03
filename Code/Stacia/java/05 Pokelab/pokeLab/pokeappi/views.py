92182eccb4027e30f249e00767cbb9d05bdd7ec9
Stacia <staciaAnn.aguilar@gmail.com>
2021-08-02 18:30:54 -0700

    <3

--- Code/Stacia/java/05 Pokelab/pokeLab/pokeappi/views.py
+++ Code/Stacia/java/05 Pokelab/pokeLab/pokeappi/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Pokemon
 from django.db.models import Q
 from django.http import JsonResponse
+from django.core.paginator import Paginator
 # pokeLab\pokeappi\views.py
 # pokeLab\pokeappi\management\commands\pokemon.py
 
@@ -10,27 +11,36 @@ def index(request):
 
 
 def pokeappi(request):
-    
 
     search_term = request.GET.get('search_term', '')
+    page = request.GET.get('page', '1')
+    page = int(page) if page.isdigit() else 1
+    limit = request.GET.get('limit', '3')
+    limit = int(limit) if limit.isdigit() else 3
 
 
-    poke_data = Pokemon.objects.all().order_by('id').filter(Q(name__icontains=search_term)|Q(types__icontains=search_term))
 
-    for pokemon in poke_data:
-            pokedex_data.append({
+    pokedata = Pokemon.objects.all().order_by('id').filter(Q(name__icontains=search_term)|Q(types__icontains=search_term))
+
+    paginator = Paginator(pokedata, limit)
+    if page > paginator.num_pages:
+        pokedata = []
+    else:
+        pokedata = paginator.page(page)
+
+    pokedex =[]
+    for pokemon in pokedata:
+            pokedex.append({
                 'name' : pokemon.name,
                 'number' : pokemon.number,
                 'height' : pokemon.height,
                 'weight' : pokemon.weight,
                 'image_front': pokemon.image_front,
                 'image_back': pokemon.image_back,
-                'type': pokemon.type.split()
+                'types': pokemon.types.split()
                 })
-
     
-    
-    return JsonResponse({'poke_data':pokedex_data})
+    return JsonResponse({"pokedex":pokedex, 'total_pages': paginator.num_pages})
 
     # paginator = Paginator(pokemon, limit)
     
