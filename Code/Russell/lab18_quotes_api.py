5d88d194a0a6d77f0af81bb99bbdc888b3019823
Russell Fraze <russell.fraze@gmail.com>
2021-08-08 14:47:27 -0700

    Lab 18 Quotes Api Updated

--- Code/Russell/lab18_quotes_api.py
+++ Code/Russell/lab18_quotes_api.py
@@ -29,25 +29,24 @@ data_list = data['quotes']
 for thing in data_list:
     print(thing['body'])
     
-#Ask the user for their next move and adjust the search accordingly    
-next_move = input('Type next to see the next page, or type a new word')
-if next_move == 'next':
-    page += 1
-elif next_move != 'next':
-    query = next_move
+#Ask the user for their next move and adjust the search accordingly 
+while True:   
+    next_move = input('Type next to see the next page, or type a new word')
+    if next_move == 'next':
+        page += 1
+    elif next_move != 'next':
+        query = next_move
 
+    url = f'https://favqs.com/api/quotes?page={page}&filter={query}'
 
+    header = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
+    response = requests.get(url, headers= header)
+    data = response.json()
 
-url = f'https://favqs.com/api/quotes?page={page}&filter={query}'
+    data_list = data['quotes']
 
-header = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
-response = requests.get(url, headers= header)
-data = response.json()
-
-data_list = data['quotes']
-
-for thing in data_list:
-    print(thing['body'])
+    for thing in data_list:
+        print(thing['body'])
 
 
 
