e97bf41f39425e04214ffec108bf63593f001abc
Russell Fraze <russell.fraze@gmail.com>
2021-10-20 12:04:17 -0700

    Added comments

--- Code/Russell/lab15_raindata.py
+++ Code/Russell/lab15_raindata.py
@@ -2,18 +2,20 @@ import re
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+#Open rainfall data file 
 with open('cascade.rain.txt', 'r') as file:
     text = file.read()
-# print(text[:700])
 
 
-# dates_list 
+
+#Find date and daily total within data 
 
 regex_date = r'(\d+-[A-Z]{3}-\d{4}) +(\d+)'
 
 date_result = re.findall(regex_date, text)
 
 
+#Append the result to a list and format 
 data_list =[]
 for i in range(len(date_result)):
     data_list.append({ 'date' : date_result[i][0], 'total'  : date_result[i][1] })
@@ -24,12 +26,18 @@ for i in range(len(data_list)):
 for i in range(len(data_list)):
     data_list[i]['total'] = int(data_list[i]['total'])
 
+
+
+#Calculate the average and the variance 
+
 def average(nums):
     total = 0
     for i in range(len(data_list)):
         total += data_list[i]['total']
     return(total) / len(nums)
 
+mean = average(data_list)
+print(mean)
 
 
 def variance(nums, mean):
@@ -38,17 +46,17 @@ def variance(nums, mean):
         total += (nums[i]['total'] - mean)**2
     return total / len(nums)
 
+the_var = variance(data_list, mean)
+print(the_var)
 
-mean = average(data_list)
 
-
-
+#Find the highest total rainfall 
 seq = []
 for x in data_list:
     seq.append(x['total'])
 
  
-#Iterate over the data list to find the day with the highest rainfall 
+#Iterate over the data list to find the day the highest rainfall occured 
 highest = max(seq)
 for i in range(len(data_list)):
     if data_list[i]['total'] == highest:
@@ -56,6 +64,8 @@ for i in range(len(data_list)):
 
 
 
+
+#Create a chart with the dates and daily totals 
 x_vs = []
 for i in range(len(data_list)):
     x_vs.append(data_list[i]['date'])
